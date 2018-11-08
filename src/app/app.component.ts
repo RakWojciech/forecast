b3fa0f54bc132113ac0351478f8c4bae97d4962e
Wojtek <admin@Wojteks-iMac.local>
2018-11-08 12:48:52 +0100

    add graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {CityIdService} from './cityid.service';
 import {HttpClient} from '@angular/common/http';
+import * as Chart from 'chart.js';
 
 @Component({
 	selector: 'app-root',
@@ -20,28 +21,97 @@ export class AppComponent implements OnInit {
 	loading = false;
 	avgPressure = [];
 	avg;
+	@ViewChild('donut') donut: ElementRef;
+
 	constructor(private http: HttpClient, private _cityId: CityIdService) {
 
 	}
 
 	ngOnInit() {
+
 		this._cityId.getCityId().subscribe(data => {
 			this.city = data;
 
 			this.cityList();
 		});
 	}
+
 	cityList() {
-		this.city.forEach( e => {
-			this.cityArray.push( e.name);
+		this.city.forEach(e => {
+			this.cityArray.push(e.name);
 		});
 	}
+
+	displayChart(temp) {
+		let tempArray = [];
+		let dateArray = [];
+		// tempArray.push(temp);
+		// console.log(temp);
+		temp.forEach( e => {
+			tempArray.push(e.main.temp);
+			dateArray.push(e.dt_txt);
+		});
+		// console.log(dateArray);
+
+		const donutCtx = this.donut.nativeElement.getContext('2d');
+
+		const data = {
+			labels: dateArray,
+			datasets: [
+				{
+					data: tempArray,   // Example data
+					backgroundColor: [
+						'#00b4ff',
+					],
+					borderColor: [
+						'#00b4ff',
+					],
+					borderWidth: 1
+				}]
+		};
+
+		const chart = new Chart(
+			donutCtx,
+			{
+				type: 'line',
+				data: data,
+				options: {
+					legend: {
+						display: false,
+						labels: {
+							fontColor: '#fff'
+						}
+					},
+					scales: {
+						yAxes: [{
+							ticks: {
+								fontColor: "#fff",
+								fontSize: 16,
+								// stepSize: 1,
+								beginAtZero: true
+							}
+						}],
+						xAxes: [{
+							ticks: {
+								fontColor: "#fff",
+								fontSize: 16,
+								// stepSize: 1,
+								beginAtZero: true
+							}
+						}]
+					}
+				}
+			}
+		);
+	}
+
 	calculateDayAvgPressure(e) {
-		console.log(e );
+		console.log(e);
 		// e.forEach( el => {
 		// 	console.log(el.main.pressure);
 		// });
 	}
+
 	submit() {
 		this.loading = true;
 		this.city.forEach(e => {
@@ -81,7 +151,7 @@ export class AppComponent implements OnInit {
 				});
 				setTimeout(() => {
 					this.forecast = this.displayDays;
-
+					this.displayChart(this.forecast);
 					// console.log(this.displayDays);
 					this.loading = false;
 				}, 500);
